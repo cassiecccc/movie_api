8226e031e6eadee9e051635952462d9cd30a260c
cassiecccc <cassieducc@gmail.com>
2023-04-13 18:34:15 -0400

    updated endpoints and documentation

--- index.js
+++ index.js
@@ -1,67 +1,138 @@
 const express = require("express"),
-  morgan = require("morgan");
-fs = require("fs");
-path = require("path");
+  morgan = require("morgan"),
+  app = express(),
+  uuid = require("uuid"),
+  bodyParser = require("body-parser"),
+  fs = require("fs"),
+  path = require("path");
 
-let topMovies = [
+app.use(bodyParser.json());
+let users = [
   {
-    title: "Se7en",
-    director: "David Fincher",
+    id: 1,
+    name: "Cassie",
+    favoriteMovies: ["Interstellar"],
   },
   {
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-  },
-  {
-    title: "Reservoir Dogs",
-    director: "Quentin Tarantino",
-  },
-  {
-    title: "Fight Club",
-    director: "David Fincher",
-  },
-  {
-    title: "Her",
-    director: "Spike Jonze",
-  },
-  {
-    title: "Catch Me If You Can",
-    director: "Steven Spielberg",
-  },
-  {
-    title: "Identity",
-    director: "James Mangold",
-  },
-  {
-    title: "Shutter Island",
-    director: "Martin Scorsese",
-  },
-  {
-    title: "A Beautiful Mind",
-    director: "Ron Howard",
-  },
-  {
-    title: "The Legend of 1900",
-    director: "Giuseppe Tornatore",
-  },
-  {
-    title: "The Best Offer",
-    director: "Giuseppe Tornatore",
-  },
-  {
-    title: "Cinema Paradiso",
-    director: "Giuseppe Tornatore",
-  },
-  {
-    title: "The Invisible Guest",
-    director: "Oriol Paulo",
-  },
-  {
-    title: "American Psycho",
-    director: "Mary Harron",
+    id: 2,
+    name: "Alex",
+    favoriteMovies: [],
   },
 ];
-const app = express();
+
+let movies = [
+  {
+    Title: "Avatar",
+    Year: "2009",
+    Rated: "PG-13",
+    Released: "18 Dec 2009",
+    Runtime: "162 min",
+    Genre: {
+      Name: "Fantasy",
+      Description:
+        "fantasy, also spelled phantasy, imaginative fiction dependent for effect on strangeness of setting (such as other worlds or times) and of characters (such as supernatural or unnatural beings).",
+    },
+    Director: {
+      Name: "James Cameron",
+      Bio: "James Francis Cameron CC (born August 16, 1954) is a Canadian filmmaker. A major figure in the post-New Hollywood era, he is considered one of the industry's most innovative filmmakers, regularly pushing the boundaries of cinematic capability with his use of novel technologies. He first gained recognition for writing and directing The Terminator (1984) and found further success with Aliens (1986), The Abyss (1989), Terminator 2: Judgment Day (1991), and the action comedy True Lies (1994). He wrote and directed Titanic (1997), Avatar (2009) and its sequels, with Titanic earning him Academy Awards for Best Picture, Best Director and Best Film Editing. A recipient of various other industry accolades, two of his films have been selected for preservation in the National Film Registry by the Library of Congress.",
+    },
+    Writer: "James Cameron",
+    Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    Plot: "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    Language: "English, Spanish",
+    Country: "USA, UK",
+    Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+    Metascore: "83",
+    imdbRating: "7.9",
+    imdbVotes: "890,617",
+    imdbID: "tt0499549",
+    Type: "movie",
+    Response: "True",
+    Images: [
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNzM2MDk3MTcyMV5BMl5BanBnXkFtZTcwNjg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY2ODQ3NjMyMl5BMl5BanBnXkFtZTcwODg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxOTEwNDcxN15BMl5BanBnXkFtZTcwOTg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMDg1Nzk1MV5BMl5BanBnXkFtZTcwMDk0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+    ],
+  },
+  {
+    Title: "The Wolf of Wall Street",
+    Year: "2013",
+    Rated: "R",
+    Released: "25 Dec 2013",
+    Runtime: "180 min",
+    Genre: {
+      Name: "Crime",
+      Description:
+        "As the name implies, the crime genre is largely classified by a story that is centered around the solving of a crime. The story needs a protagonist, usually some type of detective, whether a professional or an amateur or even a private investigator, who is determined to solve the crime",
+    },
+    Director: {
+      Name: "Martin Scorsese",
+      Bio: "Martin Charles Scorsese (/skɔːrˈsɛsi/ skor-SESS-ee,[2] Italian: [skorˈseːze, -eːse]; born November 17, 1942) is an American film director, producer, screenwriter and actor. Scorsese emerged as one of the major figures of the New Hollywood era. He is the recipient of many major accolades, including an Academy Award, four BAFTA Awards, three Emmy Awards, a Grammy Award, three Golden Globe Awards, and two Directors Guild of America Awards. He has been honored with the AFI Life Achievement Award in 1997, the Film Society of Lincoln Center tribute in 1998, the Kennedy Center Honor in 2007, the Cecil B. DeMille Award in 2010, and the BAFTA Fellowship in 2012.",
+    },
+    Writer: "Terence Winter (screenplay), Jordan Belfort (book)",
+    Actors: "Leonardo DiCaprio, Jonah Hill, Margot Robbie, Matthew McConaughey",
+    Plot: "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.",
+    Language: "English, French",
+    Country: "USA",
+    Awards: "Nominated for 5 Oscars. Another 35 wins & 154 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMjIxMjgxNTk0MF5BMl5BanBnXkFtZTgwNjIyOTg2MDE@._V1_SX300.jpg",
+    Metascore: "75",
+    imdbRating: "8.2",
+    imdbVotes: "786,985",
+    imdbID: "tt0993846",
+    Type: "movie",
+    Response: "True",
+    Images: [
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNDIwMDIxNzk3Ml5BMl5BanBnXkFtZTgwMTg0MzQ4MDE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0NzAxODAyMl5BMl5BanBnXkFtZTgwMDg0MzQ4MDE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTExMDk1MDE4NzVeQTJeQWpwZ15BbWU4MDM4NDM0ODAx._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg3MTY4NDk4Nl5BMl5BanBnXkFtZTgwNjc0MzQ4MDE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgzMTg4MDI0Ml5BMl5BanBnXkFtZTgwOTY0MzQ4MDE@._V1_SY1000_CR0,0,1553,1000_AL_.jpg",
+    ],
+  },
+  {
+    Title: "Interstellar",
+    Year: "2014",
+    Rated: "PG-13",
+    Released: "07 Nov 2014",
+    Runtime: "169 min",
+    Genre: {
+      Name: "Sci-Fi",
+      Description:
+        "Science fiction (sometimes shortened to sf or sci-fi) is a genre of speculative fiction, which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life.",
+    },
+    Director: {
+      Name: "Christopher Nolan",
+      Bio: "Christopher Edward Nolan CBE (born 30 July 1970) is a British-American filmmaker. Known for his Hollywood blockbusters with complex storytelling, Nolan is considered a leading filmmaker of the 21st century. His films have grossed $5 billion worldwide. The recipient of many accolades, he has been nominated for five Academy Awards, five BAFTA Awards and six Golden Globe Awards. In 2015, he was listed as one of the 100 most influential people in the world by Time, and in 2019, he was appointed Commander of the Order of the British Empire for his contributions to film.",
+    },
+    Writer: "Jonathan Nolan, Christopher Nolan",
+    Actors: "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+    Plot: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    Language: "English",
+    Country: "USA, UK",
+    Awards: "Won 1 Oscar. Another 39 wins & 134 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+    Metascore: "74",
+    imdbRating: "8.6",
+    imdbVotes: "937,412",
+    imdbID: "tt0816692",
+    Type: "movie",
+    Response: "True",
+    Images: [
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA3NTEwOTMxMV5BMl5BanBnXkFtZTgwMjMyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMzQ5ODE2MzEwM15BMl5BanBnXkFtZTgwMTMyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg4Njk4MzY0Nl5BMl5BanBnXkFtZTgwMzIyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMzE3MTM0MTc3Ml5BMl5BanBnXkFtZTgwMDIyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNjYzNjE2NDk3N15BMl5BanBnXkFtZTgwNzEyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+    ],
+  },
+];
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
@@ -74,8 +145,122 @@ app.get("/", (req, res) => {
   res.send("Welcome to my movieapi!");
 });
 
+//new user register || Create
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("users need names");
+  }
+});
+
+//update username || Create
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+  let user = users.find((user) => user.id == id);
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("user does not exist");
+  }
+});
+
+//update favorite movie || Update
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send("user does not exist");
+  }
+});
+
+//delete favorite movie || Delete
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (title) => title !== movieTitle
+    );
+    res
+      .status(200)
+      .send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send("user does not exist");
+  }
+});
+
+//deregister
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    users = users.filter((user) => user.id != id);
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send("user does not exist");
+  }
+});
+
+//return a list of movie || Read
+
 app.get("/movies", (req, res) => {
-  res.json(topMovies);
+  res.status(200).json(movies);
+});
+
+//return a single movie || Read
+
+app.get("/movies/:title", (req, res) => {
+  // const title = req.params.title; common form below;
+  const { title } = req.params;
+  const movie = movies.find((movie) => movie.Title === title);
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("no such movie");
+  }
+});
+
+//return genre || Read
+
+app.get("/movies/genre/:genreName", (req, res) => {
+  // const title = req.params.title; common form below;
+  const { genreName } = req.params;
+  const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("no such genre");
+  }
+});
+
+//return director || Read
+
+app.get("/movies/directors/:directorName", (req, res) => {
+  // const title = req.params.title; common form below;
+  const { directorName } = req.params;
+  const director = movies.find(
+    (movie) => movie.Director.Name === directorName
+  ).Director;
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("no such director");
+  }
 });
 
 //error handling
@@ -85,7 +270,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong!");
 });
 
-app.use(express.static("public"));
+app.use("/documentation", express.static("public/documentation.html"));
 
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
