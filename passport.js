7231d5fb2639a53f72a197e6865f5bf0af261ebe
cassiecccc <cassieducc@gmail.com>
2023-04-21 11:38:01 -0400

    updated findOne()

--- passport.js
+++ passport.js
@@ -15,22 +15,21 @@ passport.use(
     },
     (username, password, callback) => {
       console.log(username + "  " + password);
-      Users.findOne({ Username: username }, (error, user) => {
-        if (error) {
-          console.log(error);
+      Users.findOne({ Username: username })
+        .then((user) => {
+          if (!user) {
+            console.log("incorrect username");
+            return callback(null, false, {
+              message: "Incorrect username or password.",
+            });
+          }
+          console.log("finished");
+          return callback(null, user);
+        })
+        .catch((err) => {
+          console.error(err);
           return callback(error);
-        }
-
-        if (!user) {
-          console.log("incorrect username");
-          return callback(null, false, {
-            message: "Incorrect username or password.",
-          });
-        }
-
-        console.log("finished");
-        return callback(null, user);
-      });
+        });
     }
   )
 );
