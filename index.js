dfb3795c26c335eda4aae6728bc2f18718c7755e
cassiecccc <cassieducc@gmail.com>
2023-04-21 21:26:22 -0400

    updated mongodb

--- index.js
+++ index.js
@@ -36,7 +36,12 @@ const Models = require("./model.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect("mongodb://localhost:27017/mfDB", {
+// mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
