fc80e6baf89b311ee01829da68b7937288ad12f1
cassiecccc <cassieducc@gmail.com>
2023-05-12 12:14:32 -0400

    updated mongoose

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const Models = require("./model.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-// mongo "mongodb+srv://cluster0.vejeyvd.mongodb.net" --username myFlixDBadmin
-
 const express = require("express"),
   morgan = require("morgan"),
   uuid = require("uuid"),
@@ -48,13 +46,18 @@ const { check, validationResult } = require("express-validator");
 //   useUnifiedTopology: true,
 // });
 
-mongoose.connect(
-  "mongodb+srv://myFlixDBadmin:Sn5ALhght7IVS0a7@cluster0.vejeyvd.mongodb.net/myFlixDB?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// mongoose.connect(
+//   "mongodb+srv://myFlixDBadmin:Sn5ALhght7IVS0a7@cluster0.vejeyvd.mongodb.net/myFlixDB?retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
+
+mongoose.connect(process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(morgan("combined", { stream: accessLogStream }));
 
@@ -234,17 +237,6 @@ app.get("/movies", (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
-
-app.get("/users", (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.status(200).json(users);
     })
     .catch((err) => {
       console.error(err);
