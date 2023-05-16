2902b1aa08f683829973306d785a6d22d880e90a
cassiecccc <cassieducc@gmail.com>
2023-05-16 10:02:32 -0400

    removed /movies auth

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ app.delete(
 
 app.get(
   "/movies",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Movies.find()
       .then((movies) => {
