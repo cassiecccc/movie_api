93935a4a9666b0a86c95c87f2392f85115963442
cassiecccc <cassieducc@gmail.com>
2023-05-22 10:48:52 -0400

    updated /users

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post(
     // if (!errors.isEmpty()) {
     //   return res.status(422).json({ errors: errors.array() });
     // }
-    // let hashedPassword = Users.hashPassword(req.body.Password);
+    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOne({ Username: req.body.Username })
       .then((user) => {
         if (user) {
