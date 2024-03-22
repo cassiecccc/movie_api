bfc3019057537b4517351a4a0fa3cb23a6ed592f
cassiecc <cassieducc@gmail.com>
2024-03-22 13:35:28 -0400

    update cors

--- index.js
+++ index.js
@@ -20,27 +20,28 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-let allowedOrigins = [
-  "http://localhost:8080/",
-  "http://testsite.com",
-  "http://localhost:1234/",
-];
+// let allowedOrigins = [
+//   "http://localhost:8080/",
+//   "http://testsite.com",
+//   "http://localhost:1234/",
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          "The Cors policy for this app doesn't allow access from origin: " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message =
+//           "The Cors policy for this app doesn't allow access from origin: " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 let auth = require("./auth")(app);
 const passport = require("passport");
